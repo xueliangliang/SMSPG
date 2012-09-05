ac023284a7b7da7708942b2a7cadfab171aa6f73
xueliangliang <xueliangliang@hotmail.com>
2012-09-05 20:21:59 +0800

    Design a event notification model
    1. User uses http to send sms
    2. Server will receive the request and send to mobile agent
    3. At the same time it will add an event listener for the uniqueid of
    the request.
    4. It send back result to the request user
    
    At the same time "MESSAGE_LENGTH" is added in protocol for easier
    parsing

--- server/server.js
+++ server/server.js
@@ -2,12 +2,12 @@
 * TCP Protocol
 *
 * - Receive SMS
-*   - <UNIQUEID> RECV_SMS_REQ <FROM> <MESSAGE>
-*   - <UNIQUEID> RECV_SMS_RESP <STATUS>  
+*   - <UNIQUEID> RECV_SMS_REQ <FROM> <MESSAGE_LENGHT> <MESSAGE>\4\4
+*   - <UNIQUEID> RECV_SMS_RESP <STATUS> \4\4
 *
 * - Send SMS
-*   - <UNIQUEID> SEND_SMS_REQ <FROM> <TO> <MESSAGE>
-*   - <UNIQUEID> SEND_SMS_RESP <STATUS>
+*   - <UNIQUEID> SEND_SMS_REQ <FROM> <TO> <MESSAGE_LENGTH> <MESSAGE>\4\4
+*   - <UNIQUEID> SEND_SMS_RESP <STATUS>\4\4
 *
 * - UNIQUEID can use current time stamp in millisec
 * 
@@ -36,8 +36,13 @@ var server = net.createServer(function(c) { //'connection' listener
     delete activeSocket;
     console.log('server disconnected');
   });
-  c.write('hello\r\n');
   activeSocket = c;
+  c.on('data', function(data) {
+	var text = data.toString();
+	console.log('Data Received:'+text);
+	var uniqueid=text.substring(0,text.indexOf(' '));
+	c.emit('data'+uniqueid,text);
+  });
 });
 server.listen(8124, function() { //'listening' listener
   console.log('server bound');
@@ -60,15 +65,21 @@ http.createServer(function (req, res) {
   var query = querystring.parse(srvUrl.query);
   
 
+  res.writeHead(200, {'Content-Type': 'text/plain'});
   try{
 	if(activeSocket !== undefined) {
 		console.log("Socket Write");
-		activeSocket.write((new Date()).getTime()+" "+SEND_SMS_REQ+" "+query.from+" "+query.to+" "+query.message+"\r\n");
+		var uniqueid = (new Date()).getTime();
+		activeSocket.write(uniqueid+" "+SEND_SMS_REQ+" "+query.from+" "+query.to+" "+query.message+"\r\n");
+		var callback = function(text){
+	          console.log('data'+uniqueid+' event received');
+		  res.end(text+'\n');
+		  activeSocket.removeListener('data'+uniqueid,callback);
+		}
+		activeSocket.on('data'+uniqueid,callback);
 	}
   }catch(err) {
 	console.log(err);
   }
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
 }).listen(1337, '127.0.0.1');
 console.log('Server running at http://127.0.0.1:1337/');
--- server/testclient.js
+++ server/testclient.js
@@ -4,7 +4,8 @@ var options = {
   host: 'localhost',
   port: 1337,
   path: '/sendsms?from=1231231&to=1231231&message=hello%20world',
-  method: 'GET'
+  method: 'GET',
+  timeout: 30000
 };
 
 var req = http.request(options, function(res) {
@@ -15,6 +16,8 @@ var req = http.request(options, function(res) {
     console.log('BODY: ' + chunk);
   });
 });
+
+req.setTimeout(30000);
 
 req.on('error', function(e) {
   console.log('problem with request: ' + e.message);
